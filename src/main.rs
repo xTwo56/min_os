1029b6d6f770fa01332c89da117aea021d137c5f
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2026-05-22 16:06:59 +0530

    push vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+pub mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -12,14 +14,7 @@ static HELLO: &[u8] = b"shoutout to sangeet";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("shoutout to sangeet{}", "!");
 
     loop {}
 }
