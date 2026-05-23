dfeb676a1f8e751ec4cc3b5678aecf416e079be3
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2026-05-23 23:30:08 +0530

    enable testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 pub mod vga_buffer;
 
@@ -16,5 +19,23 @@ static HELLO: &[u8] = b"shoutout to sangeet";
 pub extern "C" fn _start() -> ! {
     println!("shoutout to sangeet{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
