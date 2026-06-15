2043eb321149745fbada3f2e4fcf357684a04b09
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2026-06-15 06:31:51 +0530

    add cpu exceptions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
+pub mod interrupt;
 pub mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -19,10 +21,35 @@ static HELLO: &[u8] = b"shoutout to sangeet";
 pub extern "C" fn _start() -> ! {
     println!("shoutout to sangeet{}", "!");
 
+    init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+fn init() {
+    interrupt::init_idt();
 }
 
 #[cfg(test)]
@@ -31,6 +58,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
